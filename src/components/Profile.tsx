b8511805d1bb996044cd35783a4c0fbe7e70a7b1
Marcelo <mrclgst10@gmail.com>
2021-02-24 20:30:44 -0300

    feat: add Profile, Countdown and CompletedChallenges components

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export default function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/augustomarcelo.png" alt="Marcelo Augusto"/>
+
+      <div>
+        <strong>Marcelo Augusto</strong>
+        <p>
+          <img src="icons/level.svg" alt="Seta para cima" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  )
+}
