09f04e629d70d4c9241d2036f258c1b617de5969
Marcelo <mrclgst10@gmail.com>
2021-03-07 20:33:52 -0300

    chore: add play icon

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -53,6 +53,7 @@ export function Countdown() {
               onClick={startCountdown}
             >
               Iniciar um ciclo
+              <img src="/icons/play-arrow.svg" alt="Seta branca para a direita"/>
             </button>
           )}
         </>
