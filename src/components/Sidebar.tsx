f363beb842465c9d2c3e9e6402c728a7c62c3ee7
Marcelo <mrclgst10@gmail.com>
2021-03-08 17:08:40 -0300

    refactor: drop links to array

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,35 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '../styles/components/Sidebar.module.css';
-import Home from '../../public/icons/home.svg';
+import HomeIcon from '../../public/icons/home.svg';
+import AwardIcon from '../../public/icons/award.svg';
 
 export function Sidebar() {
   const router = useRouter();
+
+  const links = [
+    {
+      href: '/',
+      children: <HomeIcon />
+    },
+    {
+      href: '/leaderbord',
+      children: <AwardIcon />
+    },
+  ]
 
   return (
     <aside className={styles.container}>
       <img src="/logo.svg" alt="Três colunas azuis com a do meio maior do que as demais" />
       <nav>
         <ul>
-          <li className={router.pathname === '/' ? styles.activeLink : ''}>
-            <Link href="/">
-              <a>
-                <Home />
-              </a>
-            </Link>
-          </li>
-          <li className={router.pathname === '/ranking' ? styles.activeLink : ''}>
-            <Link href="/ranking">
-              <a>
-                <img src="/icons/award.svg" alt="Medalha"/>
-              </a>
-            </Link>
-          </li>
+          {links.map(link => (
+            <li key={link.href} className={router.pathname === link.href ? styles.activeLink : ''}>
+              <Link href={link.href}>
+                <a>{link.children}</a>
+              </Link>
+            </li>  
+          ))}
         </ul>
       </nav>
     </aside>
